0ed79841c506766827f502b9aa1f0edb0d5fbd0a
Archit Choudhary <archit.digisocial@gmail.com>
2022-07-27 21:36:51 +0530

    Implemented vowelsCounter

---
+++ level 1/vowelsCounter/index.js
@@ -0,0 +1,38 @@
+/*
+ An iterative approach to counting the number of vowels in a string of text.
+ */
+
+//  const vowels = ["a", "e", "i", "o", "u"]
+
+//  function vowelsCounter(text) {
+//      // Initialize counter
+//      let counter = 0;
+ 
+//      // Loop through text to test if each character is a vowel
+//      for (let letter of text.toLowerCase()) {
+//          if (vowels.includes(letter)) {
+//              counter++
+//          }
+//      }
+ 
+//      // Return number of vowels
+//      return counter
+//  }
+
+
+ // REGULAR EXPRESSIONS
+
+function vowelsCounter(text) {
+    // Search text with Regex and store all matching instances 
+    let matchingInstances = text.match(/[aeiou]/gi);
+
+    // Check if matching instances exist then calculate length
+    if (matchingInstances) {
+        // Return number of vowels
+        return matchingInstances.length
+    } else {
+        return 0
+    }
+}
+
+module.exports = vowelsCounter;
